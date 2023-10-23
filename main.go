1d82e1ca655841d7654c71e43a072cc97a1a6fb4
dick <18616774735@163.com>
2023-10-23 22:28:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func HandlerPing(ctx *gin.Context) {
-	// return data object hello world world
+	// return data object hello world
 	ctx.JSON(http.StatusOK, gin.H{
 		"code":    0,
 		"message": "pong",
